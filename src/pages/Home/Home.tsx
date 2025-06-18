2130d284ccecf84855df7526b58d86467fd25a50
xtoazt <mynameisrohanandthisismyemail@gmail.com>
2025-06-18 17:24:14 -0400

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -152,10 +152,7 @@ export function Home({ userId }: HomeProps) {
         }}
       >
         <Typography variant="body1">
-          This is a free communication tool that is designed for simplicity,
-          privacy, and security. All interaction between you and your online
-          peers is encrypted. There is no record of your conversation once you
-          all leave.
+          Welcome to Real V2. All data is secure, not collected, and anonymous.
         </Typography>
       </Box>
       <Box
@@ -167,7 +164,7 @@ export function Home({ userId }: HomeProps) {
         }}
       >
         <MuiLink
-          href="https://github.com/jeremyckahn/chitchatter"
+          href="https://github.com/xtoazt/real-v2"
           target="_blank"
           sx={() => ({
             color: theme.palette.text.primary,
@@ -186,14 +183,14 @@ export function Home({ userId }: HomeProps) {
       <Typography variant="body1" sx={{ textAlign: 'center', mb: 1 }}>
         Licensed under{' '}
         <MuiLink
-          href="https://github.com/jeremyckahn/chitchatter/blob/develop/LICENSE"
+          href="https://github.com/xtoazt/real-v2/blob/develop/LICENSE"
           target="_blank"
         >
           GPL v2
         </MuiLink>
         . Please{' '}
         <MuiLink
-          href="https://github.com/jeremyckahn/chitchatter/blob/develop/README.md"
+          href="https://github.com/xtoazt/real-v2/blob/develop/README.md"
           target="_blank"
         >
           read the docs
